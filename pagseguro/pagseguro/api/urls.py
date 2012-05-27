daa9587c8381fc5e36bff77e091b0f3ae0229da1
Andrews Medina <andrewsmedina@gmail.com>
2012-05-27 03:41:58 -0300

    fixed checkout view.
    using csrf_exempt on view and fixed the xml output.

--- pagseguro/pagseguro/api/urls.py
+++ pagseguro/pagseguro/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^checkout/$', 'pagseguro.api.views.checkout', name='checkout'),
+    url(r'^checkout$', 'pagseguro.api.views.checkout', name='checkout'),
 )
--- pagseguro/pagseguro/api/views.py
+++ pagseguro/pagseguro/api/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def checkout(request):
-    output = """
-<?xml version="1.0" encoding="ISO-8859-1"?>
+    output = """<?xml version="1.0" encoding="ISO-8859-1"?>
 <checkout>
 <code>8CF4BE7DCECEF0F004A6DFA0A8243412</code>
 <date>2010-12-02T10:11:28.000-02:00</date>
